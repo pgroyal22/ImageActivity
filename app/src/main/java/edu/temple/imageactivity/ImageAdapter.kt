c62deefbd85851d32d45b76d110bc8e9d915ce94
pgroyal22 <tuj57093@temple.edu>
2021-09-27 23:02:05 -0400

    custom adapter for recycler TODO: override functions

---
+++ app/src/main/java/edu/temple/imageactivity/ImageAdapter.kt
@@ -0,0 +1,21 @@
+package edu.temple.imageactivity
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageAdapter (val _context : Context, _ImageObjects : Array<ImageObject>, onItemSelectedListener: AdapterView.OnItemSelectedListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
